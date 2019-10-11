2e9e5c5e20db75f131c2966c4d488fa3783a2e5e
a-vargasmarte <a.vargasmarte@gmail.com>
2019-10-11 17:45:40 -0400

    mvp for ay!

---
+++ js/ay!.js
@@ -0,0 +1,75 @@
+let giphyEmbedLink = "https://media.giphy.com/media/";
+
+let gridData = [
+  {
+    gifLinks: [
+      { name: "hsC2YwH3RMtzBn91uQ", class: "col-md-3 gif" },
+      { name: "hqTfCVz9dT03nrHsDc", class: "col-md-3 gif" },
+      { name: "hsC2YwH3RMtzBn91uQ", class: "col-md-3 gif" }
+    ],
+    class: "row gif-row"
+  },
+  {
+    gifLinks: [
+      { name: "dy3hu2VUKFFxs2ZwzB", class: "col-md-3 gif" },
+      { name: "mGEjfBYzbInzcirJPo", class: "col-md-3 gif" },
+      { name: "ZZrk4gIW7qwb9YDFW1", class: "col-md-3 gif" }
+    ],
+    class: "row gif-row"
+  },
+  {
+    gifLinks: [
+      { name: "QX1wdUMZUIpd7ZdiEg", class: "col-md-3 gif" },
+      { name: "hTJ7jWhx4npSJqNTQF", class: "col-md-3 gif" },
+      { name: "RhGhAisyQ0F9hGC8hX", class: "col-md-3 gif" }
+    ],
+    class: "row gif-row"
+  }
+];
+
+let linkEnd = "/giphy.gif";
+
+let container = $("div.container");
+
+gridData.map(row => {
+  //   console.log(container);
+
+  // create gifRow
+  let gifRow = $(`<div class=${row.class}></div>`);
+
+  // create a col-md-3 column
+  let firstCol = $(`<div class=col-md-3></div>`);
+
+  // append firstCol to row
+  gifRow.append(firstCol);
+
+  //   console.log(gifRow);
+
+  // map through gifLinks
+  row.gifLinks.map(gif => {
+    // console.log(gif);
+    // and create divs and give appropriate attributes
+    let gifDiv = $(`<div class=${gif.class}></div>`);
+
+    // create an image and give the appropriate links
+    let image = $(
+      `<img src=${giphyEmbedLink}${gif.name}${linkEnd} width=100% />`
+    );
+
+    // append image to gifDiv
+    gifDiv.append(image);
+
+    console.log(gifDiv);
+
+    // append gifDiv to gifRow
+
+    gifRow.append(gifDiv);
+  });
+
+  // append firstCol to gifRow
+
+  gifRow.append(firstCol);
+
+  //   append gifRow to container
+  container.append(gifRow);
+});
--- js/main.js
+++ js/main.js
@@ -1,25 +1,40 @@
 // create an array of all artists
 
 const artists = [
-  { name: "Luisa Brandelli", piece: "5 Estrelas" },
-  { name: "Alina Rancier", piece: "Ay!" },
-  { name: "Elisa Bergel Melo", piece: "ok guarebel" },
-  { name: "Javier Maria", piece: "Futuro Flotante" },
-  { name: "Ray Ferreira", piece: "undipbienlazy-phantasiatransdermica" },
-  { name: "Those Girls", piece: "The Wrong" }
+  {
+    name: "Ad Minolti",
+    piece: "Play Significant Otherness",
+    outerLink: "http://minoliti.com.ar/P_S_O/web-export/"
+  },
+  { name: "Luisa Brandelli", piece: "5 Estrelas", outerLink: false },
+  { name: "Alina Rancier", piece: "Ay!", outerLink: false },
+  { name: "Elisa Bergel Melo", piece: "ok guarebel", outerLink: false },
+  {
+    name: "Javier Maria",
+    piece: "Futuro Flotante",
+    outerLink: "https://fade-cut.github.io/futuroflotante/"
+  },
+  {
+    name: "Ray Ferreira",
+    piece: "undipbienlazy-phantasiatransdermica",
+    outerLink: false
+  },
+  { name: "Those Girls", piece: "The Wrong", outerLink: false }
 ];
 
 // iteratively append artist names to #artist-list
 artists.map(artist => {
   // declare a variable that will return a lowercased and kebab cased string for each piece of the artists
 
-  let parsedPiece = artist.piece
-    .split(" ")
-    .map(d => d.toLowerCase())
-    .join("-");
+  let parsedPiece = artist.outerLink
+    ? artist.outerLink
+    : artist.piece
+        .split(" ")
+        .map(d => d.toLowerCase())
+        .join("-");
 
   let name = $(`<li class='list-group-item'>`).html(
-    `${artist.name}: <a class='btn btn-primary' href='${parsedPiece}'>${artist.piece} </a>`
+    `${artist.name}: <a class='btn btn-primary' href='${parsedPiece}.html'>${artist.piece} </a>`
   );
   $("#artist-list").append(name);
 });
